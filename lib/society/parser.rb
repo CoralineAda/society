ec96e9dbd927412bad84b8bc6d92593e0ce34834
Bantik <corey@idolhands.com>
2014-10-30 12:13:01 -0500

    begin wrapping analyst

--- lib/society/parser.rb
+++ lib/society/parser.rb
@@ -1,39 +1,43 @@
-require 'fukuzatsu'
+require 'analyst'
 
 module Society
 
-  class Parser < Fukuzatsu::Parser
+  class Parser
 
-    attr_reader :parsed_files
+    attr_reader :start_path
 
     def initialize(start_path)
-      super(start_path, :none, 0)
-      @parsed_files = parse_files
+      @start_path = start_path
+    end
+
+    def analyzer
+      @analyzer ||= ::Analyst.new(self.start_path)
     end
 
     def class_graph
       @class_graph ||= begin
         graph = ObjectGraph.new
-        graph.nodes = parsed_files.map do |parsed_file|
+        graph.nodes = analyzer.classes.map do |klass|
           Node.new(
-            name: parsed_file.class_name,
-            address: parsed_file.path_to_file,
-            edges: parsed_file.class_references
+            name: klass.full_name,
+            address: "", #TODO"
+            edges: [] # TODO parsed_file.class_references
           )
         end
         graph
       end
     end
 
+    # TODO pass in class name, don't assume #first
     def method_graph
       @method_graph ||= begin
         graph = ObjectGraph.new
-        target = parsed_files.first
-        graph.nodes = target.methods.map do |method|
+        target = analyzer.classes.first
+        graph.nodes = target.all_methods.map do |method|
           Node.new(
             name: method.name,
-            address: target.class_name,
-            edges: method.references
+            address: "", #target.class_name,
+            edges: [] #method.references
           )
         end
         graph
