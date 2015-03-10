ea9934f248efd171f36fdcd05b814b634b218de2
Tina Wuest <tina@wuest.me>
2015-03-10 14:09:50 -0500

    Update ObjectGraph, Edge, Node classes, add tests
    The ObjectGraph class is updated to behave primarily as an Array, which
    will be expected to contain only Node objects.  Initialization is
    overridden to allow any number of Nodes or lists of Nodes to be passed
    to the constructor.  The #+ and #push/#<< methods are overridden to
    implement behavior more consistent with how the graph is expected to
    handle Nodes.
    
    The Node class has been introduced to store data about any given vertex
    on the graph (specifically, resolved and unresolved edges and
    metainformation to be used in edge resolution).  Intersecting nodes can
    be summed to produce a new Node containing the the aggregated data from
    them.  Non-intersecting nodes will produce nil when summed.
    
    The Edge class is updated to only track the destination (directionality
    of the edges is achieved by having the edge be owned by the origin node)
    and the edge's weight.  Adding two edges produces a new edge with their
    weights summed if the edges share their destination; otherwise nil is
    returned.

---
+++ lib/society/node.rb
@@ -0,0 +1,86 @@
+module Society
+
+  # The Node class represents a single node in a graph.  In this case, nodes
+  # are assumed to be either Classes or Modules.
+  class Node
+
+    attr_reader :name, :type, :edges, :unresolved, :meta
+
+    # Public: Creates a new node.
+    #
+    # name       - Name to be assigned to the node.  Assumed to be a Class or
+    #              Module name.
+    # type       - Type of node.  Assumed to be :class or :module.
+    # edges      - Edges which point to other nodes.
+    # unresolved - References to nodes which have not yet been resolved.
+    #              (default: [])
+    # meta       - Information to be tracked about the node itself.
+    #              (default: [])
+    def initialize(name:, type:, edges: [], unresolved: [], meta: [])
+      @name       = name
+      @type       = type
+      @edges      = edges
+      @unresolved = unresolved
+      @meta       = meta
+    end
+
+    # Public: Reports whether another node intersects.
+    # Nodes are considered to be intersecting if their name and type are equal.
+    #
+    # node - Another Node.
+    #
+    # Returns true or false.
+    def intersects?(node)
+      node.name == name && node.type == type
+    end
+
+    # Public: Create a node representing the sum of the current node and
+    # another, intersecting node.
+    #
+    # node - Another node object.
+    #
+    # Returns self if self is passed.
+    # Returns nil if the nodes do not intersect.
+    # Returns a new node containing the sum of both nodes' edges otherwise.
+    def +(node)
+      return self if self == node
+      return nil unless self.intersects?(node)
+
+      new_edges      = accumulate_edges(edges, node.edges)
+      new_unresolved = accumulate_edges(unresolved, node.unresolved)
+      new_meta       = meta + node.meta
+
+      return Node.new(name: name, type: type, edges: new_edges,
+                      unresolved: new_unresolved, meta: new_meta)
+    end
+
+    # Public: Return the name of the node.
+    #
+    # Returns a string.
+    def to_s
+      name.to_s
+    end
+    alias_method :inspect, :to_s
+
+    private
+
+    # Internal: Reduce two lists of edges to a single list with equivalent
+    # edges summed.
+    #
+    # edges_a - Array of Edges.
+    # edges_b - Array of Edges.
+    #
+    # Returns an array of Edges.
+    def accumulate_edges(edges_a, edges_b)
+      new_edges = (edges_a + edges_b).flatten.reduce([]) do  |edges, edge|
+        if edges.detect { |e| e.to == edge.to }
+          edges.map { |e| e + edge || edge }
+        else
+          edges + [edge]
+        end
+      end
+    end
+
+  end
+
+end
